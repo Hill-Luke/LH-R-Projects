ec488727f1ea9adc98690eb73581250ca27db6e4
Luke Hill <luke.hill246@gmail.com>
2019-01-23 17:48:18 -0500

    Reordering the dashboard and adding the mental health tab

--- Dashboard_sample.R
+++ Dashboard_sample.R
@@ -24,37 +24,37 @@ library(plotly)
 internetuse_all<-read_csv("/Users/lukeofthehill/Desktop/R/Data/Internet Usage/usage_all.csv")
 groups<-read_csv("/Users/lukeofthehill/Desktop/R/Data/Internet Usage/groups.csv")
 
-  #Subsetting to 1990 through 2016
-  internetuse<-subset(internetuse_all, select=c('Country Name','Country Code','1990':'2016')) %>% data.frame()
-  
-  #melting the data to format it in long format.
-  long_use<-melt(internetuse,id=c('Country.Name', 'Country.Code'), measured=c('X1990':'X2016'))
-  long_use$Year<-substr(long_use$variable, 2, 5) %>% as.integer()
-  long_use$pct_usage<-long_use$value
-  long_use$value<-NULL
-  groups$Country.Code<-groups$'Country Code'
-  groups$'Country Code'<-NULL
-  groups_sub<-subset(groups, select=c('Country.Code','Region', 'IncomeGroup'))
-  long_use<-subset(long_use, select=c('Country.Name','Country.Code','time','pct_usage')) %>% data.frame()
-  internet_reformat<-left_join(long_use,groups_sub, by='Country.Code', copy=FALSE)
+#Subsetting to 1990 through 2016
+internetuse<-subset(internetuse_all, select=c('Country Name','Country Code','1990':'2016')) %>% data.frame()
 
-  world_internet_usage<-sqldf("select Region, AVG(pct_usage) as average_usage, Year 
-                              from internet_reformat
-                              group by Region, Year;")
-  
-  world_internet_usage<-subset(world_internet_usage, is.na(Region)==FALSE & Year>=1990)
-  
-  
+#melting the data to format it in long format.
+long_use<-melt(internetuse,id=c('Country.Name', 'Country.Code'), measured=c('X1990':'X2016'))
+long_use$Year<-substr(long_use$variable, 2, 5) %>% as.integer()
+long_use$pct_usage<-long_use$value
+long_use$value<-NULL
+groups$Country.Code<-groups$'Country Code'
+groups$'Country Code'<-NULL
+groups_sub<-subset(groups, select=c('Country.Code','Region', 'IncomeGroup'))
+long_use<-subset(long_use, select=c('Country.Name','Country.Code','time','pct_usage')) %>% data.frame()
+internet_reformat<-left_join(long_use,groups_sub, by='Country.Code', copy=FALSE)
+
+world_internet_usage<-sqldf("select Region, AVG(pct_usage) as average_usage, Year 
+                            from internet_reformat
+                            group by Region, Year;")
+
+world_internet_usage<-subset(world_internet_usage, is.na(Region)==FALSE & Year>=1990)
+
+
 #Reading in the Tech Adoption in the US data
-  tech_adopt_USA<-read_csv("/Users/lukeofthehill/Desktop/R/Data/tech adoption/tech_adoption.csv")
-  tech_adopt_USA$adoptpct<-rename(tech_adopt_USA$`Technology Diffusion (Comin and Hobijn (2004) and others) (%)`, tech_adopt_USA$adoptpct)
+tech_adopt_USA<-read_csv("/Users/lukeofthehill/Desktop/R/Data/tech adoption/tech_adoption.csv")
+tech_adopt_USA$adoptpct<-rename(tech_adopt_USA$`Technology Diffusion (Comin and Hobijn (2004) and others) (%)`, tech_adopt_USA$adoptpct)
 #Reading in Mobile Money outside of the US.
-  mobile_money<-read_csv("/Users/lukeofthehill/Desktop/R/Data/tech adoption/mobile_money.csv")
- 
-  
+mobile_money<-read_csv("/Users/lukeofthehill/Desktop/R/Data/tech adoption/mobile_money.csv")
+
+
 #Reading in the Anxiety disorders in Men and Women in the US data
-  anxiety<-read_csv("/Users/lukeofthehill/Desktop/R/Data/anxiety.csv")
-  US_anxiety<-subset(anxiety, Entity=="United States" & Year>=1990)
+anxiety<-read_csv("/Users/lukeofthehill/Desktop/R/Data/anxiety.csv")
+US_anxiety<-subset(anxiety, Entity=="United States" & Year>=1990)
 
 #Reading in the GDP and Country Group Data
 gdp_all_country<-read_csv("/Users/lukeofthehill/Desktop/R/Data/GDP per cap.csv")
@@ -67,8 +67,8 @@ gdp_melt$Year<-substr(gdp_melt$variable, 2, 5) %>% as.integer()
 
 gdp_melt$'GDP Per Capita'<-as.integer(gdp_melt$value)
 gdp_melt$variable<-NULL
-  gdp_melt$value<-NULL
-  
+gdp_melt$value<-NULL
+
 #Joining with the country group data
 gdp_full<-left_join(gdp_melt, select(country_group,"Country.Name", "Region", "Country.Code"), by=c("Country.Name", "Country.Code"))
 
@@ -76,121 +76,19 @@ gdp_full<-left_join(gdp_melt, select(country_group,"Country.Name", "Region", "Co
 gdp_int<-left_join(gdp_full, select(internet_reformat, "Country.Code","pct_usage", "Year"), by=c("Country.Code","Year"))
 
 
-#################################
-#Defining the dashboard structure.
-#################################
-ui <- dashboardPage(
-  dashboardHeader(title="Example Dashboard"),
-  dashboardSidebar(
-    #Sidebar List of pages
-    sidebarMenu(
-      menuItem("Home", tabName = "home", icon=icon("dashboard")),
-      menuItem("Technology", tabName = "Technology", icon=icon("dashboard")),
-      menuItem("Mental Health", tabName = "Mental Health", icon=icon("dashboard"))
-      )
-  ),
-  #Defining the body content for each page
-  dashboardBody(
-    tabItems(
-    #Defining what is going on each of the tabs
-      #Home
-      tabItem(
-        tabName="home",
-          #creating the content
-          fluidRow(
-              box("Welcome to my sample dashboard. This dashboard was created using R Shinydashboard. 
-                  Data were obtained from various public sources, all originally from Our World In Data.
-                  The specific data sources are listed in each tab. For more information, please visit the following URL:
-                  <https://ourworldindata.org>", width=12))
-              ),
-      #Internet Usage Tab
-      tabItem(tabName="Technology",
-        #creating the content
-        fluidRow(
-          box(
-            "This is the Technology Usage Tab", width=12
-            ),
-          box(
-            plotlyOutput("world_intusage_plot"), width=12
-            ),
-          box(
-            plotlyOutput("mobile_money_plot"), width=12
-            ),
-          box(plotlyOutput("gdp_world"), width=12, sliderInput(inputId = "year_gdp", "Year", min=1, max=27, value=1))
-              )),
-      #Technology Adoption
-      tabItem(
-        tabName="Mental Health",
-        #creating the content
-        fluidRow(
-          box("This is the Mental Health Tab", width=12))
-      )
-    )
-  )
-)
 
 
 
-#################################
-#Running the application.
-#################################
 
 
-server <- function(input, output) { 
-  
-  output$world_intusage_plot<-renderPlotly({  
-    
-  world_intusage_plot<-ggplot(world_internet_usage, aes(x=Year, y=`Mean Internet Usage (%)`, color=Region))+
-    geom_line(stat="identity") +
-    ylab("Average Percent of Population using the Internet") +
-    scale_y_continuous(limits=c(0,1), labels=scales::percent)
-    })
-  
-  output$mobile_money_plot<-renderPlotly({
-    
-      mobile_money$num_accounts<-as.integer(mobile_money$`Registered mobile money accounts (GSMA (2017)) (registered accounts)`)
-      mobile_money_sub<-subset(mobile_money, Entity!="World" & Entity!="World (excluding Sub-Saharan Africa)" & Year >=2011)
-      #Number of Mobile Money Accounts
-      mobile_money_plot<-ggplot(mobile_money_sub, aes(x=Year, y=num_accounts, color=Entity)) +
-        geom_area(stat="identity", aes(fill=Entity))+
-        scale_y_continuous()
-    #  ggplotly(mobile_money_plot)
-      })
-  output$gdp_world<-renderPlotly({
-    slider<-sqldf("select distinct Year from gdp_int;")
-    slider$slider<-c(1:27)
-    GDP_year<-left_join(gdp_int,slider, by="Year") %>% data.frame()
-  
-    GDP_year$filter<-c(input$year_gdp)
-    #GDP_year$filter<-c(7)
-    
-    
-      #subsetting to the filter year
-    graph_GDP<-subset(GDP_year, slider==GDP_year$filter) %>% data.frame()
-    
-     #Renaming vars
-    graph_GDP$`GDP Per Capita`<-graph_GDP$GDP.Per.Capita
-    graph_GDP$`Internet Usage (%)`<-graph_GDP$pct_usage/100
-    
-    GDP_Int_plot<-ggplot(graph_GDP, aes(x=`GDP Per Capita`, y=`Internet Usage (%)`, color=Region))+
-       geom_point(stat="identity")+
-       ylab("Percent of Population using the Internet")+
-       scale_y_continuous(limits=c(0,1))
-      ggplotly(GDP_Int_plot)
-      
-  })
-  
-    }
 
-shinyApp(ui, server)
 
-#ggplotly(world_intusage_plot)
 
 ###
 #Subset this data to the tech you want to make it more legible.
 table(tech_adopt_USA$Entity)
 tech_sub<-subset(tech_adopt_USA, (Entity=="Social media usage"| Entity=="Smartphone usage"
-| Entity=="Tablet" | Entity=="Internet" | Entity=="Computer") & Year>=1990)
+                                  | Entity=="Tablet" | Entity=="Internet" | Entity=="Computer") & Year>=1990)
 
 graph_anx<-left_join(tech_sub, US_anxiety, by="Year")
 
@@ -277,7 +175,7 @@ EatingDisorder_SM<- graph_anx %>%
   geom_point(stat="identity") +  scale_colour_gradient(low = "steelblue", high = "yellow") +
   theme_bw()
 #+ scale_fill_brewer(palette="Set1")
-  #scale_fill_manual(values=c("#48D1CC"))
+#scale_fill_manual(values=c("#48D1CC"))
 plot(EatingDisorder_SM)
 
 ggplotly(EatingDisorder_SM)
@@ -287,5 +185,128 @@ Depression_SM<- graph_anx %>%
   ggplot(aes(x=Year, y=`Depression....`, size=`Technology Diffusion (Comin and Hobijn (2004) and others) (%)`))+
   geom_point()
 ggplotly(Depression_SM)
+
+
+
+
+
+
+
+
+#################################
+#Defining the dashboard structure.
+#################################
+
+
+sidebar<- dashboardSidebar(
+  #Sidebar List of pages
+  sidebarMenu(
+    menuItem("Home", tabName = "home", icon=icon("dashboard")),
+    menuItem("Technology", tabName = "Technology", icon=icon("dashboard")),
+    menuItem("Mental Health", tabName = "MentalHealth", icon=icon("dashboard"))
+  )
+)
+#Defining the body content for each page
+dashbody<-dashboardBody(
+  tabItems(
+    #Defining what is going on each of the tabs
+    #Home
+    tabItem(
+      tabName="home",
+      #creating the content
+      fluidRow(
+        box("Welcome to my sample dashboard. This dashboard was created using R Shinydashboard. 
+            Data were obtained from various public sources, all originally from Our World In Data.
+            The specific data sources are listed in each tab. For more information, please visit the following URL:
+            <https://ourworldindata.org>", width=12
+        )
+        )
+        ),
+    #Internet Usage Tab
+    tabItem(tabName="Technology",
+            #creating the content
+            fluidRow(
+              box(
+                "This is the Technology Usage Tab", width=12
+              ),
+              box(
+                plotlyOutput("world_intusage_plot"), width=12
+              ),
+              box(
+                plotlyOutput("mobile_money_plot"), width=12
+              ),
+              box(
+                plotlyOutput("gdp_world"), width=12, sliderInput(inputId = "year_gdp", "Year", min=1, max=27, value=1)
+              )
+            )
+    ),
+    tabItem(tabName = "MentalHealth",
+            fluidRow(
+              box("This is the Mental Health Tab", width=12)
+            )
+    )
+        ))
+
+
+ui <- dashboardPage(
+  dashboardHeader(title="Example Dashboard"),
+  sidebar,
+  dashbody)
+
+#################################
+#Running the application.
+#################################
+
+
+server <- function(input, output) { 
+  
+  output$world_intusage_plot<-renderPlotly({  
+    
+    world_intusage_plot<-ggplot(world_internet_usage, aes(x=Year, y=`Mean Internet Usage (%)`, color=Region))+
+      geom_line(stat="identity") +
+      ylab("Average Percent of Population using the Internet") +
+      scale_y_continuous(limits=c(0,1), labels=scales::percent)
+  })
+  
+  output$mobile_money_plot<-renderPlotly({
+    
+    mobile_money$num_accounts<-as.integer(mobile_money$`Registered mobile money accounts (GSMA (2017)) (registered accounts)`)
+    mobile_money_sub<-subset(mobile_money, Entity!="World" & Entity!="World (excluding Sub-Saharan Africa)" & Year >=2011)
+    #Number of Mobile Money Accounts
+    mobile_money_plot<-ggplot(mobile_money_sub, aes(x=Year, y=num_accounts, color=Entity)) +
+      geom_area(stat="identity", aes(fill=Entity))+
+      scale_y_continuous()
+    #  ggplotly(mobile_money_plot)
+  })
+  output$gdp_world<-renderPlotly({
+    slider<-sqldf("select distinct Year from gdp_int;")
+    slider$slider<-c(1:27)
+    GDP_year<-left_join(gdp_int,slider, by="Year") %>% data.frame()
+    
+    GDP_year$filter<-c(input$year_gdp)
+    #GDP_year$filter<-c(7)
+    
+    
+    #subsetting to the filter year
+    graph_GDP<-subset(GDP_year, slider==GDP_year$filter) %>% data.frame()
+    
+    #Renaming vars
+    graph_GDP$`GDP Per Capita`<-graph_GDP$GDP.Per.Capita
+    graph_GDP$`Internet Usage (%)`<-graph_GDP$pct_usage/100
+    
+    GDP_Int_plot<-ggplot(graph_GDP, aes(x=`GDP Per Capita`, y=`Internet Usage (%)`, color=Region))+
+      geom_point(stat="identity")+
+      ylab("Percent of Population using the Internet")+
+      scale_y_continuous(limits=c(0,1))
+    ggplotly(GDP_Int_plot)
+    
+  })
+  
+}
+
+shinyApp(ui, server)
+
+#ggplotly(world_intusage_plot)
+
 
 
